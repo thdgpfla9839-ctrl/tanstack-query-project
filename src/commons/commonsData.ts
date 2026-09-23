2c5c4332a2a77a5b5f73e68e9f793fc3e90028a0
thdgpfla9839-ctrl <thdgpfla9839@gmail.com>
2026-09-23 12:49:51 +0900

    유투브 api 추가

--- src/commons/commonsData.ts
+++ src/commons/commonsData.ts
@@ -108,3 +108,21 @@ export interface FoodDetailItem {
     jjimcount: number,
     hit: number
 }
+export interface YoutubeItem {
+
+    id:{
+        videoId: string,
+    };
+    snippet:{
+        title: string,
+        description: string,
+        thumbnail:{
+            medium:{
+                url: string,
+            }
+        }
+    }
+}
+export interface YoutubeResponse {
+    items: YoutubeItem[];
+}
